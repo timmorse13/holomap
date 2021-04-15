4a76c6a61f7241663d92712d2586755faad440fb
Edward Arana <Edwardaranadlt@gmail.com>
2021-04-15 13:44:47 -0700

    finished the characters models

---
+++ models/Characters.js
@@ -0,0 +1,95 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Characters extends Model {}
+
+Characters.init(
+  {
+    id: {
+
+      type: DataTypes.INTEGER,
+
+      allowNull: false,
+
+      primaryKey: true,
+
+      autoIncrement: true,
+
+    },
+    name: {
+
+      type: DataTypes.STRING(2000),
+
+      allowNull: false,
+
+    },
+    gender: {
+
+      type: DataTypes.STRING(2000),
+
+
+    },
+    skin_color: {
+      type: DataTypes.STRING(2000),
+      
+      allowNull: true,
+    },
+    hair_color: {
+
+      type: DataTypes.STRING(2000),
+
+      allowNull: true,
+
+    },
+    height: {
+
+      type: DataTypes.STRING(2000),
+
+      allowNull: true,
+
+    },
+    eye_color: {
+
+      type: DataTypes.STRING(100),
+
+      allowNull: true,
+
+    },
+    mass: {
+
+      type: DataTypes.STRING,
+
+      allowNull: true,
+
+    },
+    homeworld: {
+
+        type: DataTypes.STRING,
+
+        allowNull: true,
+    },
+    birth_year: {
+
+        type: DataTypes.STRING,
+
+        allowNull: true,
+    }
+
+
+  },
+  {
+    sequelize,
+
+    timestamps: false,
+
+    freezeTableName: true,
+
+    underscored: true,
+
+    modelName: 'Characters',
+
+  }
+);
+
+module.exports = Characters;
