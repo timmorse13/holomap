9d92ac4cbadf0546b163516d452d3d994aa56863
Cody Regis <codyregis6891@yahoo.com>
2021-04-16 14:33:29 -0700

    added connection, server, db, edited models, etc

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-// const express = require("express");
+const express = require('express');
+
+// Import the connection object
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to the database before starting the Express.js server
+sequelize.sync({force: false}).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
 
 // const mongoose = require("mongoose");
 // const routes = require("./routes");
