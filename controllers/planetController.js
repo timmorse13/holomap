196a5e14f98ad6ba0b5fa2282b534215df4cb5d7
timmorse13 <timmorse13@gmail.com>
2021-04-19 12:00:55 -0700

    controller files

---
+++ controllers/planetController.js
@@ -0,0 +1,12 @@
+// const { Planet } = require("../models")
+
+// import { create, destroy, findByPk } from "../models/Planets";
+
+// create
+
+// destroy
+
+
+// findByPk (req.params.name)
+
+// find pass in a where {req.params.name} clause for planets
