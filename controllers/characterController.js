f9b0fca200738d925d50998317fe9a2cd989dbd3
timmorse13 <timmorse13@gmail.com>
2021-04-19 14:27:55 -0700

    routes work!

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,1 +1,16 @@
-// const { Character } = require("../models")
+const { Characters } = require("../models");
+
+module.exports = {
+    findAll: function(req, res) {
+        Characters.find(req.query)
+        .then(dbCharacters => res.json(dbCharacters))
+        .catch(err => res.status(422).json(err));
+    },
+    findbyPk: function(req, res) {
+        console.log('findByPk')
+        Characters.findByPk(req.params.id)
+        .then(dbCharacters => res.json(dbCharacters))
+        .catch(err => res.status(422).json(err));
+    }
+};
+
