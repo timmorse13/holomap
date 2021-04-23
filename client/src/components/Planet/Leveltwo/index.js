84510cee1c3152e0018de11039024a8fbbb93e0b
timmorse13 <timmorse13@gmail.com>
2021-04-23 13:38:00 -0700

    toggle between surface and planet

--- client/src/components/Planet/Leveltwo/index.js
+++ client/src/components/Planet/Leveltwo/index.js
@@ -4,13 +4,13 @@ import React from "react";
 
 function LevelTwo({ name, diameter, rotation_period, orbital_period, planet_info, img, surface }) {
     return (
-        <div style={{backgroundImage: `/${surface}`}} >
+        <div  >
             {name} <br />
             {diameter} <br />
             {rotation_period}<br /> 
             {orbital_period}<br /> 
             {planet_info} <br />
-            {img && !surface ? (<img src={`/${img}`} alt=''/>) : (<img src={`/${surface}`} alt=''/>) }
+            {img && !surface ? (<img src={`/${img}`} alt=''/>) : (<img src={`/${surface}`} alt='' style={{backgroundImage: `/${surface}`}}/>) }
 
             {/* <Orbit/> */}
 
