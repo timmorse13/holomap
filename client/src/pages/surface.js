c9e1daa453536ff9bb745409287c321e985b024e
timmorse13 <timmorse13@gmail.com>
2021-04-23 15:25:17 -0700

    carousel

--- client/src/pages/surface.js
+++ client/src/pages/surface.js
@@ -1,43 +1,14 @@
-import React, { useState, useEffect } from "react";
-import Surface from "../components/Surface";
-import API from "../utils/Api";
+import React from "react";
 
-
-function GetSurface() {
-    const [surface, setSurface] = useState([])
-    const [surfaces, setSurfaces] = useState([])
-
-    useEffect(() => {
-        Fsurface()
-    }, [])
-
-    const handleSurfaceClick = (e) => {
-    const surfaceName = e.target.getAttribute("surface-name")
-    setSurface(surfaceName)
-    console.log(surfaceName)
-}
-
-    const Fsurface = () => {
-        
-        API.getPlanet(surface)
-        .then(res =>  {
-            console.log(res);
-            setSurfaces(res.data)
-            console.log(surfaces)
-        })
-        .catch(() => setSurface([])
-
-        );
-    };
-
+function Gallspin() {
     return (
         <div>
-            <Surface 
-            handleSurfaceClick={handleSurfaceClick}
-            img={surfaces.img}
-            />
+
+
+
+
         </div>
     )
 }
 
-export default GetSurface;
+export default Gallspin
