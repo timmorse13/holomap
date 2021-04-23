9f4e08de50a961ac9ee2154fc42680c423fd9975
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-04-22 22:04:02 -0700

    Page layout and initial styling
    Added fonts folder, resized galaxy view planets

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,108 +1,67 @@
-import React, { useState, useEffect, Component } from "react";
-import API from "../utils/Api"
-import Characters from "../components/Characters";
-import Planets from "../components/Planets";
-import LevelTwo from "../components/Planet/Leveltwo"
-import Planet from "../components/Planet";
-import Galaxy from "../components/Galaxy/Galaxy"
-
+import React, { useState, useEffect, Component } from 'react';
+import API from '../utils/Api';
+import Characters from '../components/Characters';
+import Planets from '../components/Planets';
+import LevelTwo from '../components/Planet/Leveltwo';
+import Planet from '../components/Planet';
+import Galaxy from '../components/Galaxy/Galaxy';
 
 function Home() {
-  const [planet, setPlanet] = useState([])
-  const [planets, setPlanets] = useState([])
+	const [planet, setPlanet] = useState([]);
+	const [planets, setPlanets] = useState([]);
 
-  useEffect(() => {
-    getPlanet()
-    // loadPlanets()
-  }, [planet])
+	useEffect(() => {
+		getPlanet();
+		// loadPlanets()
+	}, [planet]);
 
-  const handlePlanetClick = (e) => {
-    const planetName = e.target.getAttribute("data-name")
-    console.log(planetName)
-    setPlanet(planetName)
+	const handlePlanetClick = (e) => {
+		const planetName = e.target.getAttribute('data-name');
+		console.log(planetName);
+		setPlanet(planetName);
+	};
+	const getPlanet = () => {
+		// console.log(name)
+		API.getPlanet(planet)
+			.then((res) => {
+				setPlanets(res.data);
+			})
+			.catch(() => setPlanets([]));
+	};
 
-  }
-  const getPlanet = () => {
-    // console.log(name)
-    API.getPlanet(planet)
-    .then(res =>  {
-      setPlanets(res.data)
-      
-    })
-    .catch(() => setPlanets([])
+	// const loadPlanets = () => {
+	//   API.getPlanets()
+	//   .then(res => {
+	//     setPlanets(res.data)
+	//   })
+	//   .catch(() => setPlanets([]))
+	// }
 
-    );
-  }
-
-
-    // const loadPlanets = () => {
-    //   API.getPlanets()
-    //   .then(res => {
-    //     setPlanets(res.data)
-    //   })
-    //   .catch(() => setPlanets([]))
-    // }
-
-
-
-
-
-return (
-  <div className ="viewport">
-        <main className = "One">
-          <LevelTwo
-  
-  img={planets.img}
-  
-  />
-
-
-            </main>
-          <div className = "Two">
-            look at dis
-            <Galaxy 
-            
-            handlePlanetClick = {handlePlanetClick}
-            // name={planet.handlePlanetClick}
-            />
-
-            
-            
-            
-          </div>
-          <div className ="Three">
-            <h3>HI ANTHONY</h3>
-            <LevelTwo
-            
-            name={planets.name}
-            diameter={planets.diameter}
-            rotation_period={planets.rotation_period} 
-            orbital_period={planets.orbital_period} 
-            planet_info={planets.planet_info}
-
-            />
-
-
-  
-          </div>
-          </div>
-  
-                  
-        
-      )
-
-
-
-
-
-
-
-
-
-
-};
-
-
+	return (
+		<div className='viewport container'>
+			<div className='row border border-info rounded'>
+				<main className='One border border-info rounded col-9'>
+					<LevelTwo img={planets.img} />
+				</main>
+				<div className='Two border border-info col-6'>
+					<Galaxy
+						handlePlanetClick={handlePlanetClick}
+						// name={planet.handlePlanetClick}
+					/>
+				</div>
+				<div className='Three border border-info overflow-auto col-4 '>
+					<LevelTwo
+						name={planets.name}
+						diameter={planets.diameter}
+						rotation_period={planets.rotation_period}
+						orbital_period={planets.orbital_period}
+						planet_info={planets.planet_info}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
 
 // class Home extends Component {
 //   state = {
@@ -111,8 +70,7 @@ return (
 //     charID: "",
 //     message: "This works"
 //   };
-  
-  
+
 //   componentDidMount() {
 //     this.getCharacters()
 //     this.getPlanet()
@@ -126,7 +84,7 @@ return (
 //         char: res.data
 //       })
 //     })
-//       .catch(() => 
+//       .catch(() =>
 //       this.setState({
 //         char: [],
 //         message: "No work"
@@ -141,7 +99,7 @@ return (
 //         plan: res.data
 //       })
 //     })
-//     .catch(() => 
+//     .catch(() =>
 //     this.setState({
 //       plan: [],
 //       message: "No planet"
@@ -149,14 +107,12 @@ return (
 //     );
 //   };
 
-
-
 //   render() {
 //     return (
 //       <div className ="viewport">
 //       <main className = "One">
 //         {this.state.char.map(character => {
-//           return <Characters 
+//           return <Characters
 //           name={character.name}
 //           gender={character.gender}
 //           skin_color={character.skin_color}
@@ -164,9 +120,9 @@ return (
 //           hair_color={character.chair_color}
 //           description={character.description}
 //           image={character.image}
-        
+
 //           />
-          
+
 //         })}
 //           </main>
 //         <div className = "Two">
@@ -175,13 +131,13 @@ return (
 //             return <LevelTwo
 //             name={planet.name}
 //             diameter={planet.diameter}
-//             rotation_period={planet.rotation_period} 
-//             orbital_period={planet.orbital_period} 
+//             rotation_period={planet.rotation_period}
+//             orbital_period={planet.orbital_period}
 //             planet_info={planet.planet_info}
 //             img={planet.img}
 
 //           />
-//           })} 
+//           })}
 //         </div>
 //         <div className ="Three">
 //           <h3>HI ANTHONY</h3>
@@ -189,12 +145,8 @@ return (
 //         </div>
 //         </div>
 
-                
-      
 //     )
 //   }
-
-
 
 // }
 
