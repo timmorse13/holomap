cee8538467fa8275095e9f9d30c0b3ae8c401783
Edward Arana <Edwardaranadlt@gmail.com>
2021-04-25 13:18:05 -0700

    doing the profile page think its done just needs styles

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,74 +3,77 @@ import React from 'react';
 
 function Profile(){
 return(
-    <div>
+<div>
+{/* nav bar  */}
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Profile</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/Home.js">Home</a>
+          </li>
+        </ul>
+        <form className="d-flex">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+{/* Profile Image Card */}
+  <div className="card bg-dark text-white" >
+    <div className="card-body">
+    <div className="d-flex flex-column align-items-center text-center">
+            <img src="" alt="Admin" className="img-fluid img-thumbnail"/>
+            <div className="file btn btn-lg btn-primary">
+                Change Photo
+              <input type="file" name="file"/>
+            </div>
 
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Profile Page</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/Home.js">Home Page</a>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+            <div className="mt-3">
+              <h4>Wookie Man 420</h4>
+              <p className="text-secondary mb-1">Jedi Master</p>
+              <p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
+            </div>
+      </div>
     </div>
   </div>
-</nav>
 
-<div className="card bg-dark text-white" >
-  <div className="card-body">
-  <div className="d-flex flex-column align-items-center text-center">
-                    <img src="https://pbs.twimg.com/profile_images/1284600130168070144/dRh7O4Kj_400x400.jpg" alt="Admin" className="rounded-circle "/>
-                    <div class="file btn btn-lg btn-primary">
-                                Change Photo
-                                <input type="file" name="file"/>
-                            </div>
-
-
-                    <div className="mt-3">
-                      <h4>John Doe</h4>
-                      <p className="text-secondary mb-1">Full Stack Developer</p>
-                      <p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
-                    </div>
-    </div>
-    <h4 className="card-title">Wookie Man 420</h4>
-    <p className="card-text">I like stars with wars.</p>
-    <a href="#" className="btn btn-primary">Edit Avatar</a>
+{/* Form Area */}
+<form className="row g-3">
+  <div className="col-md-6">
+    <label for="inputEmail4" className="form-label">Email</label>
+    <input type="email" className="form-control" id="inputEmail4"/>
   </div>
-</div>
+  <div className="col-md-6">
+    <label for="inputPassword4" className="form-label">Password</label>
+    <input type="password" className="form-control" id="inputPassword4"/>
+  </div>
+  <div className="col-12">
+    <label for="inputAddress" className="form-label">Address</label>
+    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
+  </div>
+  <div className="col-12">
+    <label for="inputAddress2" className="form-label">Address 2</label>
+    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+  </div>
+  <div className="col-md-6">
+    <label for="inputCity" className="form-label">City</label>
+    <input type="text" className="form-control" id="inputCity"/>
+  </div>
+  <div className="col-md-2">
+    <label for="inputZip" className="form-label">Zip</label>
+    <input type="text" className="form-control" id="inputZip"/>
+  </div>
 
-
-<form action="/action_page.php" className="was-validated">
-  <div className="form-group">
-    <label for="uname">Username:</label>
-    <input type="text" className="form-control" id="uname" placeholder="Enter username" name="uname" required/>
-    <div className="valid-feedback">Valid.</div>
-    <div className="invalid-feedback">Please fill out this field.</div>
+  <div className="col-12">
+    <button type="submit" className="btn btn-primary">Edit Information</button>
   </div>
-  <div className="form-group">
-    <label for="pwd">Password:</label>
-    <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pswd" required/>
-    <div className="valid-feedback">Valid.</div>
-    <div className="invalid-feedback">Please fill out this field.</div>
-  </div>
-  <div className="form-group form-check">
-    <label className="form-check-label">
-      <input className="form-check-input" type="checkbox" name="remember" required/> I agree on blabla.
-      <div className="valid-feedback">Valid.</div>
-      <div className="invalid-feedback">Check this checkbox to continue.</div>
-    </label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
 </form>
-
 
     
 
