ec250cff2e98dd03b1e85a91a024160b7150d34e
Cody Regis <codyregis6891@yahoo.com>
2021-04-26 09:42:50 -0700

    home route on profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -44,7 +44,7 @@ return (
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/Home">Home</a>
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
           </li>
         </ul>
         <form className="d-flex">
